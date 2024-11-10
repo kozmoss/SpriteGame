622cf4b5fb1d2ceabb07294a330570b188b2789a
kozmoss <sametbuzcu@outlook.com>
2024-11-10 19:41:37 +0300

    Initial Commit

---
+++ SpriteGameTests/SpriteGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteGameTests.swift
+//  SpriteGameTests
+//
+//  Created by samet on 10.11.2024.
+//
+
+import Testing
+@testable import SpriteGame
+
+struct SpriteGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
