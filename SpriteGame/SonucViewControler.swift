271cf86abdc70fe4191639f41b4a7966cc1252ed
kozmoss <sametbuzcu@outlook.com>
2024-11-12 22:21:58 +0300

    First Commit

---
+++ SpriteGame/SonucViewControler.swift
@@ -0,0 +1,29 @@
+//
+//  SonucViewControler.swift
+//  SpriteGame
+//
+//  Created by samet on 10.11.2024.
+//
+
+import UIKit
+
+class SonucViewControler: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
